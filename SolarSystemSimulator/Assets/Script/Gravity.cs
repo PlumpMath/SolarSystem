b94f160a2347332abadbc366d5f99227c0d1d718
Aries Sanchez Sulit <aries.sulit@gmail.com>
2016-03-31 23:32:43 +0800

    Added Initial Unity3D Project. Unity3D 5.3.0 Added Initial Magnet Simulation.

---
+++ SolarSystemSimulator/Assets/Script/Gravity.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Gravity : MonoBehaviour {
+
+	public GameObject target;
+
+	/*
+	// Use this for initialization
+	void Start () {
+		Debug.LogFormat ("Gravity:{0}\n", Physics.gravity);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Physics.gravity = this.target.transform.position - this.transform.position;
+		Debug.LogFormat ("Gravity:{0}\n", Physics.gravity);
+	}
+	*/
+
+	void Awake() {
+		this.GetComponent<Rigidbody> ().useGravity = false;
+	}
+
+	void FixedUpdate() {
+		//this.GetComponent<Rigidbody> ().AddRelativeForce (this.target.transform.position - this.transform.position);
+		//this.GetComponent<Rigidbody> ().AddForce (this.target.transform.position - this.transform.position);
+		//this.GetComponent<Rigidbody> ().AddRelativeForce (Target.GetGravity(this.transform.position));
+		this.GetComponent<Rigidbody> ().AddForce (Target.GetGravity(this.transform.position));
+	}
+}
---
+++ SolarSystemSimulator/Assets/Script/Target.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Target : MonoBehaviour {
+
+	public static readonly List<Target> targets = new List<Target>();
+
+	public float Range;
+	public Transform target;
+
+	private void Awake() {
+		targets.Add (this);
+	}
+
+	private void FixedUpdate() {
+		//Debug.LogFormat ("Distance:{0}\n", Vector3.Distance(this.target.transform.position, this.transform.position));
+	}
+
+	/*
+	public static Vector3 GetGravity(Vector3 pos) {
+		float distance = float.MaxValue;
+		Vector3 result = Vector3.zero;
+		targets.ForEach (t => {
+			float currDist = Vector3.Distance(t.transform.position, pos);
+			if (currDist <= distance) {
+				distance = currDist;
+				result = t.transform.position - pos;
+			}
+		});
+
+		return result;
+	}
+	*/
+
+	public static Vector3 GetGravity(Vector3 pos) {
+		//float distance = float.MaxValue;
+		Vector3 result = Vector3.zero;
+		targets.ForEach (t => {
+			float currDist = Vector3.Distance(t.transform.position, pos);
+			if (currDist <= t.Range) {
+				result += t.transform.position - pos;
+			}
+		});
+
+		return result;
+	}
+
+	private void OnDrawGizmos() {
+		UnityEditor.Handles.color = Color.yellow;
+		UnityEditor.Handles.DrawWireDisc (this.transform.position, Vector3.back, this.Range);
+		UnityEditor.Handles.DrawWireDisc (this.transform.position, Vector3.right, this.Range);
+		UnityEditor.Handles.DrawWireDisc (this.transform.position, Vector3.left, this.Range);
+		UnityEditor.Handles.DrawWireDisc (this.transform.position, Vector3.forward, this.Range);
+		UnityEditor.Handles.DrawWireDisc (this.transform.position, Vector3.down, this.Range);
+		UnityEditor.Handles.DrawWireDisc (this.transform.position, Vector3.up, this.Range);
+	}
+}
